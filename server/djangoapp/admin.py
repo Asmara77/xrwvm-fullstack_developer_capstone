34a53ea62495093a95686a049a5c9d5193d57ca6
Sami <n4913819@gmail.com>
2024-12-02 19:21:35 -0500

    change mage

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,32 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 3
+
+class CarAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+    list_filter = ['car_make', 'type', 'year']
+    search_fields = ['name', 'car_make__name']
+
+
+admin.site.register(CarModel, CarModelAdmin)
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ['name']
+    inlines = [CarModelInline]
+
+admin.site.register(CarMake, CarMakeAdmin)
 
 # Register models here
