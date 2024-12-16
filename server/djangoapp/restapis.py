382036157fa710b5b8f5347db008bbac8671cee6
Sami <n4913819@gmail.com>
2024-12-15 20:26:35 -0500

    change made

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,14 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+backend_url = os.getenv('backend_url', default="http://localhost:3030")
+sentiment_analyzer_url = os.getenv('sentiment_analyzer_url', default="http://localhost:5050/")
 
 def get_request(endpoint, **kwargs):
-    params = ""
+    params = "&".join(f"{key}={value}" for key, value in kwargs.items())
     if(kwargs):
         for key,value in kwargs.items():
             params=params+key+"="+value+"&"
@@ -24,6 +21,7 @@ def get_request(endpoint, **kwargs):
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
+        response.raise_for_status()
         return response.json()
     except:
         # If any error occurs
@@ -32,10 +30,12 @@ def get_request(endpoint, **kwargs):
 
 # def analyze_review_sentiments(text):
 def analyze_review_sentiments(text):
-    request_url = f"{https://sentianalyzer.1p0r2z6iicfn.us-south.codeengine.appdomain.cloud}analyze/{text}"
+    request_url = f"https://sentianalyzer.1pi0wntbws1j.us-south.codeengine.appdomain.cloud/analyze/{text}"
+
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
+        response.raise_for_status()
         return response.json()
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
@@ -44,8 +44,10 @@ def analyze_review_sentiments(text):
 # def post_review(data_dict):
 def post_review(data_dict):
     request_url = f"{https://n4913819-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai}/insert_review"
+
     try:
         response = requests.post(request_url,json=data_dict)
+        response.raise_for_status()
         print(response.json())
         return response.json()
     except:
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,5 +1,4 @@
 # Uncomment the required imports before adding the code
-
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
@@ -91,13 +90,10 @@ def get_cars(request):
 
 
 #Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
-def get_dealerships(request, state="All"):
-    if(state == "All"):
-        endpoint = "/fetchDealers"
-    else:
-        endpoint = "/fetchDealers/"+state
-    dealerships = get_request(endpoint)
-    return JsonResponse({"status":200,"dealers":dealerships})
+def get_dealerships(request, state="All"): 
+    endpoint = f"/fetchDealers/{state}" if state != "All" else "/fetchDealers"
+    dealerships = get_request(endpoint) 
+    return JsonResponse({"status": 200, "dealers": dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
