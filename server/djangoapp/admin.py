f480374dfc2c92e1a6e5c4d31b16d5300d3c1bb7
Sami <b@gmail.com>
2024-12-30 16:58:12 -0500

    change made

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+
 # CarModelInline class
 class CarModelInline(admin.TabularInline):
     model = CarModel
     extra = 3
 
+
 # CarAdmin class
 class CarAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
+
 
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
@@ -16,12 +19,15 @@ class CarModelAdmin(admin.ModelAdmin):
     list_filter = ['car_make', 'type', 'year']
     search_fields = ['name', 'car_make__name']
 
+
 admin.site.register(CarModel, CarModelAdmin)
+
 
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     search_fields = ['name']
     inlines = [CarModelInline]
+
 
 admin.site.register(CarMake, CarMakeAdmin)
